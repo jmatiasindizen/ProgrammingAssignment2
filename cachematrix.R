5eeea890ff88a152c8d95122c500d861f0222b54
Javier de Matías <j.matias@indizen.com>
2014-05-21 12:42:07 +0200

    Update cachematrix.R
    Add my code comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code allow calculate inverse matrix of a input bidimensional 
+## using cached values if is possible
 
-## Write a short comment describing this function
+
+## makeCacheMatrix takes a bidimensional matrix and returns a vector
+## containing set, get, setinverse and getinverse functions
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -19,7 +21,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## Write a short comment describing this function
+## cacheSolve takes a vector like makeCacheMatrix funcitons returns
+## and return inverse matrix of input argument, using cached value 
+## if is possible
+
 
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
